669543aa30c7405d80745fc17c8d916166aed813
Alexander Belovitsky <a.belovitsky@stv.dunice.net>
2024-07-15 17:58:25 +0300

    Add two programs

---
+++ src/main/java/dev/mirrex/Values.java
@@ -0,0 +1,15 @@
+package dev.mirrex;
+
+public class Values {
+
+    public static void displayMinAndMaxValue() {
+        System.out.printf("type:%s min: %s max: %s%n", Character.TYPE,
+                (int) Character.MIN_VALUE, (int) Character.MAX_VALUE);
+        System.out.printf("type:%s min: %s max: %s%n", Byte.TYPE, Byte.MIN_VALUE, Byte.MAX_VALUE);
+        System.out.printf("type:%s min: %s max: %s%n", Short.TYPE, Short.MIN_VALUE, Short.MAX_VALUE);
+        System.out.printf("type:%s min: %s max: %s%n", Integer.TYPE, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        System.out.printf("type:%s min: %s max: %s%n", Long.TYPE, Long.MIN_VALUE, Long.MAX_VALUE);
+        System.out.printf("type:%s min: %s max: %s%n", Float.TYPE, Float.MIN_VALUE, Float.MAX_VALUE);
+        System.out.printf("type:%s min: %s max: %s%n", Double.TYPE, Double.MIN_VALUE, Double.MAX_VALUE);
+    }
+}
